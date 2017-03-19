17f647f222ba0c19e7b8499c2af37330684a75e6
Mike Schulze <mike.schulze@live.com>
2017-03-19 20:07:40 +0100

    Initial project.

---
+++ KopfbogenTool/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using GalaSoft.MvvmLight.Ioc;
+using MahApps.Metro.Controls;
+
+namespace KopfbogenTool
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            // need to register ourselves as default MetroWindow for dialogs
+            SimpleIoc.Default.Register<MetroWindow>(() => this);
+
+            InitializeComponent();
+        }
+    }
+}
---
+++ KopfbogenTool/ViewModel/MainViewModel.cs
@@ -0,0 +1,70 @@
+using System;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using KopfbogenTool.Basics;
+
+namespace KopfbogenTool.ViewModel
+{
+    /// <summary>
+    /// This class contains properties that the main View can data bind to.
+    /// </summary>
+    public class MainViewModel : ViewModelBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the MainViewModel class.
+        /// </summary>
+        public MainViewModel(IDialogService aDialogs)
+        {
+            mDialogs = aDialogs;
+
+            if (IsInDesignMode)
+            {
+                // Code runs in Blend / VS Designer --> create design time data.
+                WelcomeText = "Hello Designer!";
+            }
+            else
+            {
+                // Code runs "for real"
+                WelcomeText = "Hello World!";
+            }
+
+            ShowMessageCommand = new RelayCommand(ShowMessage);
+        }
+
+        private async void ShowMessage()
+        {
+            await mDialogs.ShowMessageDialog("A simple dialog", "This is a profound message.");
+        }
+
+        public string WelcomeText
+        {
+            get
+            {
+                return mWelcomeText;
+            }
+            set
+            {
+                Set(nameof(WelcomeText), ref mWelcomeText, value);
+            }
+        }
+        private string mWelcomeText;
+
+
+        public bool WelcomeTextVisible
+        {
+            get
+            {
+                return mWelcomeTextVisible;
+            }
+            set
+            {
+                Set(nameof(WelcomeTextVisible), ref mWelcomeTextVisible, value);
+            }
+        }
+        private bool mWelcomeTextVisible = true;
+
+        public RelayCommand ShowMessageCommand { get; private set; }
+
+        private readonly IDialogService mDialogs;
+    }
+}
